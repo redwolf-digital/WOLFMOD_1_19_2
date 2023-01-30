3a97c8d130a8197e4bf104959c58d773da0510b1
REDWOLF_DiGiTAL <worakan9012@gmail.com>
2023-01-30 15:24:22 +0700

    Initial commit

---
+++ src/main/java/net/redwolf/wolfmod/WolfModClient.java
@@ -0,0 +1,10 @@
+package net.redwolf.wolfmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WolfModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
